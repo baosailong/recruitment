fce5acef64946cd1b14cd2ac8eccafe71555cb22
bsl <1626888156@qq.com>
2024-09-30 15:45:59 +0800

    first commit

---
+++ App.java
@@ -0,0 +1,17 @@
+package com.bsl;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        String fileName = "hello.txt";
+    }
+}
